98c3f388507899808cb443cab50b527378da381c
baruide <mac2@xingjiesj.com>
2019-07-31 11:41:05 +0800

    增加UI并测试

---
+++ src/index.js
@@ -0,0 +1,27 @@
+
+import Vue from 'vue'
+import HelloWorld from './components/HelloWorld'
+const components = {
+    HelloWorld
+}
+//
+// function install (Vue) {
+//     for (const item in components) {
+//         if (components[item].name) {
+//             Vue.component(components[item].name, components[item])
+//         }
+//     }
+// }
+Object.keys(components).forEach(name => {
+    Vue.component(name, components[name])
+})
+
+// /**
+//  * Global Install
+//  */
+// if (typeof window !== 'undefined' && window.Vue) {
+//     install(window.Vue)
+// }
+export  default {
+    ...components
+}
