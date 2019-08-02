e81ce31cf38a62cdb7d84d89a88b1f50473abf6d
baruide <mac2@xingjiesj.com>
2019-08-02 10:57:37 +0800

    增加返回顶部并测试

---
+++ src/components/backtop/src/backtop.vue
@@ -0,0 +1,106 @@
+<template>
+    <transition name="el-fade-in">
+        <div
+                v-if="visible"
+                @click.stop="handleClick"
+                :style="{
+        'right': styleRight,
+        'bottom': styleBottom
+      }"
+                class="xj-backtop">
+            <slot>
+               返回顶部
+            </slot>
+        </div>
+    </transition>
+</template>
+
+<script>
+    import {throttle} from 'throttle-debounce';
+
+    export default {
+        name: 'Backtop',
+
+        props: {
+            visibilityHeight: {
+                type: Number,
+                default: 200
+            },
+            target: [String],
+            right: {
+                type: Number,
+                default: 40
+            },
+            bottom: {
+                type: Number,
+                default: 40
+            }
+        },
+
+        data() {
+            return {
+                el: null,
+                container: null,
+                visible: false
+            };
+        },
+
+        computed: {
+            styleBottom() {
+                return `${this.bottom}px`;
+            },
+            styleRight() {
+                return `${this.right}px`;
+            }
+        },
+
+        mounted() {
+            this.init();
+            this.throttledScrollHandler = throttle(300, this.onScroll);
+            this.container.addEventListener('scroll', this.throttledScrollHandler);
+        },
+
+        methods: {
+            init() {
+                this.container = document;
+                this.el = document.documentElement;
+                if (this.target) {
+                    this.el = document.querySelector(this.target);
+                    if (!this.el) {
+                        throw new Error(`target is not existed: ${this.target}`);
+                    }
+                    this.container = this.el;
+                }
+            },
+            onScroll() {
+                const scrollTop = this.el.scrollTop;
+                this.visible = scrollTop >= this.visibilityHeight;
+            },
+            handleClick(e) {
+                this.scrollToTop();
+                this.$emit('click', e);
+            },
+            scrollToTop() {
+                let el = this.el;
+                let step = 0;
+                let interval = setInterval(() => {
+                    if (el.scrollTop <= 0) {
+                        clearInterval(interval);
+                        return;
+                    }
+                    step += 10;
+                    el.scrollTop -= step;
+                }, 20);
+            }
+        },
+
+        beforeDestroy() {
+            this.container.removeEventListener('scroll', this.throttledScrollHandler);
+        }
+    };
+</script>
+<style lang="scss">
+    .xj-backtop{
+        position: fixed;
+    }
+</style>
